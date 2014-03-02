d8e682d6c01ce327834ba27773753cf228eb52bb
Ionuț Arțăriși <ionut@artarisi.eu>
2014-03-02 15:06:26 +0100

    add basic templates for index with some style

--- shorter/web.py
+++ shorter/web.py
@@ -16,10 +16,11 @@
 # along with shorter. If not, see <http://www.gnu.org/licenses/>.
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
