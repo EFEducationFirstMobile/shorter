c5d656cff3e7ac218cc41805dfb8c19f63cd4250
Ionuț Arțăriși <ionut@artarisi.eu>
2017-07-11 21:44:22 +0100

    Create a testing user on starting the server

--- run_server.py
+++ run_server.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
+from shorter.database import (
+    User,
+    db_session,
+)
 from shorter.web import app
 
 if __name__ == "__main__":
+    # makes testing easier
+    test_user_created = db_session.query(User).filter_by(
+        username='jimmy').one_or_none()
+    if not test_user_created:
+        db_session.add(
+            User(username='jimmy', password='secret'))
+        db_session.commit()
+
     app.run()
